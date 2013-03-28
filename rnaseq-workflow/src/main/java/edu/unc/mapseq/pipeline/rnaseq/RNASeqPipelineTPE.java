25cec8c72a6e9452304388825145a84d122d1f82
Jason Reilly <jdr0887@renci.org>
2013-03-28 12:09:04 -0400

    incrementing pool size

--- rnaseq-workflow/src/main/java/edu/unc/mapseq/pipeline/rnaseq/RNASeqPipelineTPE.java
+++ rnaseq-workflow/src/main/java/edu/unc/mapseq/pipeline/rnaseq/RNASeqPipelineTPE.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 public class RNASeqPipelineTPE extends ThreadPoolExecutor {
 
     public RNASeqPipelineTPE() {
-        super(20, 20, 5L, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
+        super(30, 30, 5L, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
     }
 
 }
