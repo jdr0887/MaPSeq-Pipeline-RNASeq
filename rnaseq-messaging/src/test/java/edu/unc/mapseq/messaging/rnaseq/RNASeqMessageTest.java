d23ce8ebfc853adc807c8bc9db2bc7e2fd46d2f9
Jason Reilly <jdr0887@renci.org>
2013-04-17 08:45:46 -0400

    posterity commit

--- rnaseq-messaging/src/test/java/edu/unc/mapseq/messaging/rnaseq/RNASeqMessageTest.java
+++ rnaseq-messaging/src/test/java/edu/unc/mapseq/messaging/rnaseq/RNASeqMessageTest.java
@@ -1,37 +1,37 @@
 package edu.unc.mapseq.messaging.rnaseq;
 
+import java.util.Date;
+
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
-import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.junit.Test;
 
 public class RNASeqMessageTest {
 
     @Test
     public void testQueue() {
-        // String host = "localhost";
-        String host = "biodev1.its.unc.edu";
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://" + host + ":61616");
+
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(String.format("nio://%s:61616",
+                "gnet641.its.unc.edu"));
 
         Connection connection = null;
         Session session = null;
         try {
             connection = connectionFactory.createConnection();
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createQueue("queue/RNASeq");
+            Destination destination = session.createQueue("queue/rnaseq");
             MessageProducer producer = session.createProducer(destination);
             producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            // String value =
-            // "{\"account_name\":\"jreilly\",\"entities\":[{\"entity_type\":\"Sequencer run\",\"guid\":\"45900\"},{\"entity_type\":\"Workflow run\",\"name\":\"RNASeq-jreilly-test1\"}]}";
-            String value = "{\"account_name\":\"jreilly\",\"entities\":[{\"entity_type\":\"HTSF Sample\",\"guid\":\"45906\"},{\"entity_type\":\"Workflow run\",\"name\":\"RNASeq-jreilly-test1\"}]}";
-            TextMessage message = session.createTextMessage(value);
-            producer.send(message);
+            String format = "{\"account_name\":\"rc_lbg.svc\",\"entities\":[{\"entity_type\":\"HTSFSample\",\"guid\":\"%1$d\"},{\"entity_type\":\"WorkflowRun\",\"name\":\"test-%2$s-%1$d\"}]}";
+            producer.send(session.createTextMessage(String.format(format, 314616,
+                    DateFormatUtils.ISO_DATE_FORMAT.format(new Date()))));
         } catch (JMSException e) {
             e.printStackTrace();
         } finally {
